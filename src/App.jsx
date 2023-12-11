9405bd65f3263fe37dfc54f107b4593ab96945ce
Alishow1111 <alishow1111@gmail.com>
2023-12-11 16:26:46 +0000

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
     <>
+    <Header />
+    <NavBar />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function Header() {
+
+  return (
+    <>
+    <h1>Nc-News</h1>
+    </>
+  )
+}
+
+export default Header
